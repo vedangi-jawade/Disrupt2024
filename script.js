aeba03a514992cd5f56569ec950074e62c464487
Arshad <arshadsiroha5sd@gmail.com>
2024-08-18 11:50:31 +0530

    0

--- script.js
+++ script.js
@@ -2,18 +2,6 @@
 let showbtns = document.querySelectorAll('.plus-cros-icon');
 let hiddenText =  document.querySelector('.doubt-lines-text');
 
-function hideShow() {
-    if (display === 1) {
-        hiddenText.style.display = 'none';
-        hiddenText.style.transitionDuration = "1s"
-        display = 0;
-    }
-    else {
-        hiddenText.style.display = 'block';
-        display = 1;
-    }
-}
-
 // showbtn.addEventListener("click", () => {
 //     hideShow(display)
 // }
